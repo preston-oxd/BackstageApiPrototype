0af4f4ed182a04cfe1b95316318115d32c752c1b
preston-oxd <107075640+preston-oxd@users.noreply.github.com>
2023-05-19 16:18:53 -0700

    Initial mvc project added

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,8 @@
+namespace BackstageApiPrototype.Models;
+
+public class ErrorViewModel
+{
+    public string? RequestId { get; set; }
+
+    public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+}
